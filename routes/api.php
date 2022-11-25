0ba6105f05ff5f63d267393ee709e20968db619a
tinhvd <tinhvd@hybrid-technologies.vn>
2022-11-25 16:55:38 +0700

    add generator password

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +19,13 @@ Route::post('/upload-image', function (Request $request) {
     dd($upload);
     $upload = \Illuminate\Support\Facades\Storage::disk('s3')->put("dangtinh.png","file content");
     dd($upload);
+});
+Route::get('/search-ytb',function (){
+    $encode = urlencode("đi về nhà");
+    $url ="https://suggestqueries.google.com/complete/search?json=suggestCallBack&q={$encode}&hl=en&ds=yt&client=youtube&_=1669363147310&output=toolbar";
+    $data = json_decode(utf8_encode(Http::get($url)->body()),true);
+    $output = \Illuminate\Support\Arr::get($data,1);
+    return response()->json((new \App\Http\Responses\ResponseSuccess([
+        'result' => $output
+    ]))->toArray());
 });
